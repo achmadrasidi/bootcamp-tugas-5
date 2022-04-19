26b250043bb414fcb81c5de07a8f3b01700628f1
kondelow <achmadrasidi@gmail.com>
2022-04-19 14:55:45 +0700

    soal no.1

--- soal1.js
+++ soal1.js
@@ -1,1 +1,18 @@
 // Deteksi Palindrom
+
+const cekPalindrom = (word) => {
+  if (typeof word !== "string") {
+    return "word harus berupa string";
+  }
+  let cek = "";
+  for (let i = word.length - 1; i >= 0; i--) {
+    cek += word[i];
+  }
+
+  if (word !== cek) {
+    return "Bukan Palindrom";
+  }
+  return "Palindrom";
+};
+
+console.log(cekPalindrom("malam"));
