b0564a86090951da2305ebc042546c8607b35814
kondelow <achmadrasidi@gmail.com>
2022-04-20 10:16:49 +0700

    fix testing soal1.js

--- soal1.js
+++ soal1.js
@@ -1,14 +1,16 @@
 // Deteksi Palindrom
 
 const cekPalindrom = (word) => {
+  // cek tipe data word
   if (typeof word !== "string") {
     return "word harus berupa string";
   }
   let cek = "";
+  // pembuatan variabel cek membalik kata
   for (let i = word.length - 1; i >= 0; i--) {
     cek += word[i];
   }
-
+  // cek word dengan variabel cek
   if (word.toLowerCase() !== cek.toLowerCase()) {
     return "Bukan Palindrom";
   }
@@ -16,3 +18,5 @@ const cekPalindrom = (word) => {
 };
 
 console.log(cekPalindrom("malam"));
+console.log(cekPalindrom("makan"));
+console.log(cekPalindrom(10));
